c9e2d705b603711204c792002c5b19f5137c45b2
Brian Chan <brian.chan@liferay.com>
2012-08-03 15:25:53 -0700

    LPS-28950 Source formatting

--- webs/drools-web/docroot/WEB-INF/src/com/liferay/portal/bi/rules/drools/servlet/DroolsServletContextListener.java
+++ webs/drools-web/docroot/WEB-INF/src/com/liferay/portal/bi/rules/drools/servlet/DroolsServletContextListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -24,10 +24,11 @@ import org.mvel2.MVELRuntime;
  */
 public class DroolsServletContextListener implements ServletContextListener {
 
-	public void contextInitialized(ServletContextEvent servletContextEvent) {
-	}
-
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
 		MVELRuntime.resetDebugger();
 	}
+
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+	}
+
 }
