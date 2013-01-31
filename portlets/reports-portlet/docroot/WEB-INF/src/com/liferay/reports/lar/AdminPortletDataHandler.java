030ea128a10491cf4cdc135c6b9f9cf5590c4596
Brian Chan <brian.chan@liferay.com>
2013-01-31 00:17:40 -0800

    LPS-32713

--- portlets/reports-portlet/docroot/WEB-INF/src/com/liferay/reports/lar/AdminPortletDataHandler.java
+++ portlets/reports-portlet/docroot/WEB-INF/src/com/liferay/reports/lar/AdminPortletDataHandler.java
@@ -49,7 +49,7 @@ import javax.portlet.PortletPreferences;
 /**
  * @author Michael C. Han
  */
-public class AdminPortletDataHandlerImpl extends BasePortletDataHandler {
+public class AdminPortletDataHandler extends BasePortletDataHandler {
 
 	@Override
 	public PortletDataHandlerControl[] getExportControls() {
@@ -73,7 +73,7 @@ public class AdminPortletDataHandlerImpl extends BasePortletDataHandler {
 		throws Exception {
 
 		if (!portletDataContext.addPrimaryKey(
-				AdminPortletDataHandlerImpl.class, "deleteData")) {
+				AdminPortletDataHandler.class, "deleteData")) {
 
 			DefinitionUtil.removeByGroupId(
 				portletDataContext.getScopeGroupId());
