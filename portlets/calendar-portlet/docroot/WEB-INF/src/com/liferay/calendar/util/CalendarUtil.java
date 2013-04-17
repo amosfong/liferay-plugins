00bd87f650190147fefea917082ee2d1b46c6258
Brian Chan <brian.chan@liferay.com>
2013-04-16 17:36:17 -0700

    LPS-34211 Sort

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarUtil.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarUtil.java
@@ -244,6 +244,7 @@ public class CalendarUtil {
 			"permissions",
 			_getPermissionsJSONObject(
 				themeDisplay.getPermissionChecker(), calendar));
+		jsonObject.put("resourceGroupId", calendar.getResourceGroupId());
 		jsonObject.put("userId", calendar.getUserId());
 
 		return jsonObject;
