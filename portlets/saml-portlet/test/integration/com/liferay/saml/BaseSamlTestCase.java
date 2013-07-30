85bc1f73008f058de8dd6c36e9508804878f63ff
Will_Newbury <william.newbury@liferay.com>
2013-07-29 17:20:27 -0700

    LPS-37356: Manual SF and code cleanup

--- portlets/saml-portlet/test/integration/com/liferay/saml/BaseSamlTestCase.java
+++ portlets/saml-portlet/test/integration/com/liferay/saml/BaseSamlTestCase.java
@@ -197,23 +197,6 @@ public class BaseSamlTestCase extends PowerMockito {
 		return mockHttpServletRequest;
 	}
 
-	protected <T> T getMockPortletService(
-		Class<?> serviceUtilClass, Class<T> serviceClass) {
-
-		serviceUtilClasses.add(serviceUtilClass);
-
-		T service = mock(serviceClass);
-
-		when(
-			portletBeanLocator.locate(
-				Mockito.eq(serviceClass.getName()))
-		).thenReturn(
-			service
-		);
-
-		return service;
-	}
-
 	protected <T> T getMockPortalService(
 		Class<?> serviceUtilClass, Class<T> serviceClass) {
 
@@ -223,6 +206,23 @@ public class BaseSamlTestCase extends PowerMockito {
 
 		when(
 			portalBeanLocator.locate(
+				Mockito.eq(serviceClass.getName()))
+		).thenReturn(
+			service
+		);
+
+		return service;
+	}
+
+	protected <T> T getMockPortletService(
+		Class<?> serviceUtilClass, Class<T> serviceClass) {
+
+		serviceUtilClasses.add(serviceUtilClass);
+
+		T service = mock(serviceClass);
+
+		when(
+			portletBeanLocator.locate(
 				Mockito.eq(serviceClass.getName()))
 		).thenReturn(
 			service
