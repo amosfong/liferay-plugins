f711f3315118e18aa81e9f5ddfdde44c2faaac8c
Shinn Lok <shinn.lok@liferay.com>
2013-11-21 16:49:16 -0800

    LPS-42121 Constants

---
+++ portlets/bbb-portlet/docroot/WEB-INF/src/com/liferay/bbb/util/BBBConstants.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bbb.util;
+
+/**
+ * @author Shinn Lok
+ */
+public class BBBConstants {
+
+	public static final String API_METHOD_CREATE = "create";
+
+	public static final String API_METHOD_END = "end";
+
+	public static final String API_METHOD_IS_MEETING_RUNNING =
+		"isMeetingRunning";
+
+	public static final String API_METHOD_JOIN = "join";
+
+	public static final String API_PARAM_ATTENDEE_PW = "attendeePW";
+
+	public static final String API_PARAM_FULL_NAME = "fullName";
+
+	public static final String API_PARAM_MEETING_ID = "meetingID";
+
+	public static final String API_PARAM_MODERATOR_PW = "moderatorPW";
+
+	public static final String API_PARAM_NAME = "name";
+
+	public static final String API_PARAM_PASSWORD = "password";
+
+	public static final String API_RESPONSE_FAILED = "FAILED";
+
+	public static final String API_RESPONSE_RETURN_CODE = "returncode";
+
+	public static final String API_RESPONSE_RUNNING = "running";
+
+}
