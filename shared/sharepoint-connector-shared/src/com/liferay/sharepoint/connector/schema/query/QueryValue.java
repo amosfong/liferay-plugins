7fdde1dc54eb0a03896dca0fc53d7d1fd63b117c
Ivan Zaera Avellon <izaera@gmail.com>
2014-02-03 13:11:36 +0100

    LPS-43746 Move package ...schema.node one level up since we don't need the "node" particle any more in its name.
    Now that we don't have builders, there's no point in having an empty intermediate package named "node".

---
+++ shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/schema/query/QueryValue.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharepoint.connector.schema.query;
+
+import com.liferay.sharepoint.connector.schema.BaseNode;
+
+/**
+ * @author Iván Zaera
+ */
+public class QueryValue extends BaseNode {
+
+	public QueryValue(String value) {
+		this(Type.Text, value);
+	}
+
+	public QueryValue(Type type, String value) {
+		_type = type;
+		_value = value;
+	}
+
+	public static enum Type {
+
+		Integer, Lookup, Text
+
+	}
+
+	private Type _type;
+	private String _value;
+
+}
---
+++ shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/schema/query/ViewFieldsNode.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharepoint.connector.schema.query;
+
+import com.liferay.sharepoint.connector.schema.BaseNode;
+
+/**
+ * @author Iván Zaera
+ */
+public class ViewFieldsNode extends BaseNode {
+
+	public ViewFieldsNode(QueryFieldRef... fieldRefs) {
+		if (fieldRefs == null) {
+			_fieldRefs = _FIELD_REFS;
+		}
+		else {
+			_fieldRefs = fieldRefs;
+		}
+	}
+
+	private static final QueryFieldRef[] _FIELD_REFS = new QueryFieldRef[0];
+
+	private QueryFieldRef[] _fieldRefs;
+
+}
