c5bcab24b2a28301bb0092486b28cbb634f35362
Ivan Zaera Avellon <izaera@gmail.com>
2014-02-06 10:00:17 +0100

    LPS-43746 Prefix id field in SharepointObject and SharepointVersion

--- shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/SharepointObject.java
+++ shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/SharepointObject.java
@@ -14,8 +14,192 @@
 
 package com.liferay.sharepoint.connector;
 
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.net.URL;
+
+import java.util.Date;
+import java.util.Set;
+
 /**
  * @author Ivan Zaera
  */
 public class SharepointObject {
+
+	public SharepointObject(
+		long sharepointObjectId, URL url, String path, boolean isFolder,
+		long size, Date createdDate, Date lastModifiedDate, String author,
+		String checkedOutBy, Set<Permission> permissions) {
+
+		_sharepointObjectId = sharepointObjectId;
+		_url = url;
+		_path = path;
+		_folderPath = _getFolderPath(path);
+		_name = _getName(path);
+		_extension = _getExtension(path);
+		_isFolder = isFolder;
+		_size = size;
+		_createdDate = createdDate;
+		_lastModifiedDate = lastModifiedDate;
+		_author = author;
+		_checkedOutBy = checkedOutBy;
+		_permissions = permissions;
+	}
+
+	public String getAuthor() {
+		return _author;
+	}
+
+	public String getCheckedOutBy() {
+		return _checkedOutBy;
+	}
+
+	public Date getCreatedDate() {
+		return _createdDate;
+	}
+
+	public String getExtension() {
+		return _extension;
+	}
+
+	public String getFolderPath() {
+		return _folderPath;
+	}
+
+	public long getId() {
+		return _sharepointObjectId;
+	}
+
+	public Date getLastModifiedDate() {
+		return _lastModifiedDate;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getPath() {
+		return _path;
+	}
+
+	public Set<Permission> getPermissions() {
+		return _permissions;
+	}
+
+	public long getSize() {
+		return _size;
+	}
+
+	public URL getURL() {
+		return _url;
+	}
+
+	public boolean isFile() {
+		return !isFolder();
+	}
+
+	public boolean isFolder() {
+		return _isFolder;
+	}
+
+	@Override
+	public String toString() {
+		if (isFolder()) {
+			return "folder:" + _path;
+		}
+		else {
+			return "file:" + _path;
+		}
+	}
+
+	public static enum Permission {
+
+		VIEW_LIST_ITEMS(0x0000000000000001L),
+		ADD_LIST_ITEMS(0x0000000000000002L),
+		EDIT_LIST_ITEMS(0x0000000000000004L),
+		DELETE_LIST_ITEMS(0x0000000000000008L),
+		APPROVE_ITEMS(0x0000000000000010L), OPEN_ITEMS(0x0000000000000020L),
+		VIEW_VERSIONS(0x0000000000000040L),
+		DELETE_VERSIONS(0x0000000000000080L),
+		CANCEL_CHECKOUT(0x0000000000000100L),
+		MANAGE_PERSONAL_VIEWS(0x0000000000000200L),
+		MANAGE_LISTS(0x0000000000000800L), VIEW_FORM_PAGES(0x0000000000001000L),
+		OPEN(0x0000000000010000L), VIEW_PAGES(0x0000000000020000L),
+		ADD_AND_CUSTOMIZE_PAGES(0x0000000000040000L),
+		APPLY_THEME_AND_BORDER(0x0000000000080000L),
+		APPLY_STYLE_SHEETS(0x0000000000100000L),
+		VIEW_USAGE_DATA(0x0000000000200000L),
+		CREATE_SSC_SITE(0x0000000000400000L),
+		MANAGE_SUBWEBS(0x0000000000800000L), CREATE_GROUPS(0x0000000001000000L),
+		MANAGE_PERMISSIONS(0x0000000002000000L),
+		BROWSE_DIRECTORIES(0x0000000004000000L),
+		BROWSE_USER_INFO(0x0000000008000000L),
+		ADD_DEL_PRIVATE_WEB_PARTS(0x0000000010000000L),
+		UPDATE_PERSONAL_WEB_PARTS(0x0000000020000000L),
+		MANAGE_WEB(0x0000000040000000L),
+		USE_CLIENT_INTEGRATION(0x0000001000000000L),
+		USE_REMOTE_APIS(0x0000002000000000L),
+		MANAGE_ALERTS(0x0000004000000000L), CREATE_ALERTS(0x0000008000000000L),
+		EDIT_MY_USER_INFO(0x0000010000000000L),
+		ENUMERATE_PERMISSIONS(0x4000000000000000L);
+
+		public long getMask() {
+			return _mask;
+		}
+
+		private Permission(long mask) {
+			_mask = mask;
+		}
+
+		private final long _mask;
+
+	}
+
+	private String _getExtension(String path) {
+		int i = path.lastIndexOf(StringPool.PERIOD);
+
+		if (i == -1) {
+			return StringPool.BLANK;
+		}
+		else {
+			return path.substring(i + 1);
+		}
+	}
+
+	private String _getFolderPath(String path) {
+		int i = path.lastIndexOf(StringPool.SLASH);
+
+		if (i == 0) {
+			return StringPool.SLASH;
+		}
+		else {
+			return path.substring(0, i);
+		}
+	}
+
+	private String _getName(String path) {
+		if (path.equals(StringPool.SLASH)) {
+			return StringPool.SLASH;
+		}
+		else {
+			int i = path.lastIndexOf(StringPool.SLASH);
+
+			return path.substring(i + 1);
+		}
+	}
+
+	private String _author;
+	private String _checkedOutBy;
+	private Date _createdDate;
+	private String _extension;
+	private String _folderPath;
+	private boolean _isFolder;
+	private Date _lastModifiedDate;
+	private String _name;
+	private String _path;
+	private Set<Permission> _permissions;
+	private long _sharepointObjectId;
+	private long _size;
+	private URL _url;
+
 }
--- shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/SharepointVersion.java
+++ shared/sharepoint-connector-shared/src/com/liferay/sharepoint/connector/SharepointVersion.java
@@ -14,8 +14,67 @@
 
 package com.liferay.sharepoint.connector;
 
+import java.net.URL;
+
+import java.util.Date;
+
 /**
  * @author Ivan Zaera
  */
 public class SharepointVersion {
+
+	public SharepointVersion(
+		String sharepointVersionId, URL url, String version, String comments,
+		long size, Date createdDate, String createdBy) {
+
+		_sharepointVersionId = sharepointVersionId;
+		_url = url;
+		_version = version;
+		_comments = comments;
+		_size = size;
+		_createdDate = createdDate;
+		_createdBy = createdBy;
+	}
+
+	public String getComments() {
+		return _comments;
+	}
+
+	public String getCreatedBy() {
+		return _createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return _createdDate;
+	}
+
+	public String getId() {
+		return _sharepointVersionId;
+	}
+
+	public long getSize() {
+		return _size;
+	}
+
+	public URL getURL() {
+		return _url;
+	}
+
+	public String getVersion() {
+		return _version;
+	}
+
+	@Override
+	public String toString() {
+		return "version:" + _sharepointVersionId;
+	}
+
+	private String _comments;
+	private String _createdBy;
+	private Date _createdDate;
+	private String _sharepointVersionId;
+	private long _size;
+	private URL _url;
+	private String _version;
+
 }
