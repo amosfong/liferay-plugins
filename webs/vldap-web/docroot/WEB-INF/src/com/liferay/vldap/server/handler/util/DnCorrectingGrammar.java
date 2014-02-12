9f45c19880608f327d445c345de858b2e1ba4f89
Brian Chan <brian.chan@liferay.com>
2014-02-12 12:52:44 -0800

    LPS-44223 SF

--- webs/vldap-web/docroot/WEB-INF/src/com/liferay/vldap/server/handler/util/DnCorrectingGrammar.java
+++ webs/vldap-web/docroot/WEB-INF/src/com/liferay/vldap/server/handler/util/DnCorrectingGrammar.java
@@ -1,23 +1,19 @@
 /**
  * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *
- *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.liferay.vldap.server.handler.util;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-
-import org.apache.directory.shared.asn1.ber.Asn1Container;
 import org.apache.directory.shared.asn1.ber.grammar.AbstractGrammar;
 import org.apache.directory.shared.asn1.ber.grammar.GrammarTransition;
 import org.apache.directory.shared.asn1.ber.tlv.UniversalTag;
@@ -33,12 +29,6 @@ public class DnCorrectingGrammar extends AbstractGrammar {
 		return _instance;
 	}
 
-	protected DnCorrectingGrammar() {
-		_grammar = (LdapMessageGrammar) LdapMessageGrammar.getInstance();
-
-		_storeNameAction = new DnCorrectingStoreName();
-	}
-
 	@Override
 	public GrammarTransition getTransition(Enum state, int tag) {
 		GrammarTransition transition = _grammar.getTransition(state, tag);
@@ -50,6 +40,12 @@ public class DnCorrectingGrammar extends AbstractGrammar {
 		}
 
 		return transition;
+	}
+
+	protected DnCorrectingGrammar() {
+		_grammar = (LdapMessageGrammar)LdapMessageGrammar.getInstance();
+
+		_storeNameAction = new DnCorrectingStoreName();
 	}
 
 	private static DnCorrectingGrammar _instance = new DnCorrectingGrammar();
