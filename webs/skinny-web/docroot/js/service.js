8533c438c4eeb4b602b83fd549f050d4ca85cb9e
Brian Chan <brian.chan@liferay.com>
2014-02-25 12:41:01 -0800

    skinny-web 6.2.x support

--- webs/skinny-web/docroot/js/service.js
+++
@@ -1,9 +0,0 @@
-Liferay.Service.register("Liferay.Service.Skinny", "com.liferay.skinny.service", "skinny-web");
-
-Liferay.Service.registerClass(
-	Liferay.Service.Skinny, "Skinny",
-	{
-		getSkinnyDDLRecords: true,
-		getSkinnyJournalArticles: true
-	}
-);
